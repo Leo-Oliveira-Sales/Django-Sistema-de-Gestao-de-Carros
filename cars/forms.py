a449cb113aa44a691b78ca0ca5807c22a3e991c7
Leonardo <leo.sales.br@gmail.com>
2026-04-16 13:39:33 -0300

    Validações e melhora no html cars

--- cars/forms.py
+++ cars/forms.py
@@ -6,3 +6,18 @@ class CarModelForm(forms.ModelForm):
     class Meta:
         model = Car
         fields = '__all__'
+
+    # Validação:
+    def clean_value(self):
+        value = self.cleaned_data.get('value')
+        if value < 20000:
+            raise forms.ValidationError('O valor do carro não pode ser menor que 20.000,00!')
+        return value
+
+    # Tambem pode ser feito assim: sel.add_error('value', 'O valor do carro não pode ser menor que 20.000,00!') para adicionar um erro específico ao campo value, sem precisar levantar uma exceção de validação.
+
+    def clean_factory_year(self):
+        factory_year = self.cleaned_data.get('factory_year')
+        if factory_year < 1975:  # O primeiro carro foi inventado em 1886
+            raise forms.ValidationError('O ano de fabricação do carro não pode ser menor que 1975!')
+        return factory_year
